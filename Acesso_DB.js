71bef55f9865f49d8812aecd47d03b9eaaf0699a
isa <isa.tesch21@gmail.com>
2023-05-17 19:48:11 -0300

    O server coletou os dados que o javascript enviou e os adicionou na lista do banco de dados

--- Acesso_DB.js
+++ Acesso_DB.js
@@ -1,7 +1,9 @@
 const cors = require('cors')
 
 const express = require("express")
+
 const app = express()
+
 const mysql = require("mysql")
 
 const db = mysql.createConnection({
@@ -21,11 +23,27 @@ app.use(express.json())
 
 app.use(cors())
 
-
 app.post('/pegar_dados', (req, res) => {
     var Nome_Produto = req.body.box_item 
-    console.log(Nome_Produto)
-    res.send('Valor recebido: ' + Nome_Produto)
+    var Quantd_Produto = req.body.box_quantidade_item
+    var Preco_Produto = req.body.box_preco_produto
+    var Total_Produto = Quantd_Produto * Preco_Produto
+
+    if (Nome_Produto.length > 0 && Quantd_Produto.length > 0 && Preco_Produto > 0){
+        var sql = 'INSERT INTO compras_do_mercado (nome_produto, quantd_produto, preco_produto, total_produto) VALUES (?, ?, ?, ?)'
+        var valores_tabela = [Nome_Produto, Quantd_Produto, Preco_Produto, Total_Produto]
+        db.query(sql, valores_tabela,
+        (erro, resultado)=>{
+            if (erro){
+                console.log('Houve um erro ao inserir o dado: ', erro)
+            }
+            else{
+                console.log('Dado inserido com sucesso!')
+            }
+            db.end() // Fechamento da conexão com o banco de dados após a conclusão da consulta.
+        })
+    }
+
 })
 
 app.listen(3001, ()=>{
--- Interacao_Lista.js
+++ Interacao_Lista.js
@@ -1,16 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(){
     var btn_dados = document.getElementById('btn_coleta_dados')
-    btn_dados.addEventListener('click', x)
+    btn_dados.addEventListener('click', EnviaDadosParaServidor)
 
-    function x(){
+
+    function EnviaDadosParaServidor(){
         var input_box_item = document.getElementById('box_item').value
+        var input_box_quantd_item = document.getElementById('box_quantidade_item').value
+        var input_box_preco_produto = document.getElementById('box_preco_produto').value
+
     
         fetch('http://localhost:3001/pegar_dados', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({box_item: input_box_item}) /* Converte o objeto javascript em uma string json. box_item corresponde à chave
+            body: JSON.stringify({box_item: input_box_item, box_quantidade_item: input_box_quantd_item, box_preco_produto: input_box_preco_produto}) /* Converte o objeto javascript em uma string json. box_item corresponde à chave
             do json resultante*/
         })
         .then(response => response.text())
